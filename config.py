54602ef49bd81edb3fd11fc0d7caeb725c428c1d
lostyawolfer <lostyawolfer@gmail.com>
2025-11-23 16:02:18 +0100

    initial

---
+++ config.py
@@ -0,0 +1,140 @@
+TARGET_GUILD = 1426972810332340406
+
+def check_guild(guild_id: int) -> bool:
+    if guild_id == TARGET_GUILD:
+        return True
+    return False
+
+
+
+from dotenv import dotenv_values
+TOKEN = dotenv_values('.env').get('TOKEN')
+if not TOKEN:
+    print('there\'s no token\n'
+          'create a .env file in this directory and put in "TOKEN=..." and replace the "..." with ur bot token')
+
+
+
+
+ROLES = {
+    "admins": [
+        1427016174444613804, # the man behind the challenge
+        1433828741548740781  # moderator
+    ],
+    "new_people": [
+        1427076669264498820, # person
+        1433872100837425193  # newbie
+    ],
+    "bot": 1442086763161194597,
+    "leader": 1426973394099896507,
+    "available": 1434629510031999269,
+    "available_leader": 1434629548347232326,
+    "not_available": 1442132238660796416,
+    "in_vc": 1427076261586669688,
+    "in_vc_leader": 1427076452477702191,
+    "not_in_vc": 1442132263071645779,
+    "birthday": 1439339439762444552,
+    "inactive": 1434659281822679232
+}
+
+CHANNELS = {
+    "vc": 1426972811293098015,
+    "chat": 1426972811293098014,
+    "availability": 1434653852367585300,
+    "availability_message": 1434654321886363658,
+    "availability_reaction": 1439620553572089900,
+    "ps_link": 1426974154556702720
+}
+
+
+_EMOJI = {
+    "join": "<:join:1436503008924926052>",
+    "leave": "<:leave:1436503027937841173>",
+    "ban": "<:ban:1438882547588141118>",
+    "kick": "<:kick:1439803052826689537>",
+    "app_join": "<:newapp:1438882548829913209>",
+    "app_leave": "<:removedapp:1438882550075621538>",
+    "available": "<:available:1436525036281532449>",
+    "unavailable": "<:notavailable:1436528517956374578>",
+    "join_vc": "<:join_vc:1436503046107566181>",
+    "leave_vc": "<:leave_vc:1436528566174220289>",
+    "promotion": "<:promotion:1442087863347974294>",
+    "demotion": "<:demotion:1442087886391607376>",
+    "birthday": ""
+}
+
+_MESSAGES = {
+    "join": [
+        f"{_EMOJI['join']} Is {{mention}} really ready for this? After all, not many people have the patience to wait for 8 players to join.",
+        f"{_EMOJI['join']} {{mention}} should now prepare to die in the backdoor a hundred times as part of join process.",
+        f"{_EMOJI['join']} Don't worry {{mention}}, the lights flicker only *most* of the time.",
+        f"{_EMOJI['join']} It's time for {{mention}} to acknowledge the possibility of pain and seizure.",
+        f"{_EMOJI['join']} {{mention}} just joined! Quick, everyone look like we know what we're doing!",
+        f"{_EMOJI['join']} Uhh, hey, {{mention}}, you have any sanity? We're running low, could we please borrow some?",
+        f"{_EMOJI['join']} Let's hope {{mention}} has it all Figured out."
+    ],
+    "join_bot": [
+        f"{_EMOJI['app_join']} New digital entity alert! {{mention}} has manifested.",
+        f"{_EMOJI['app_join']} Beep boop! {{mention}} just computed its way in.",
+        f"{_EMOJI['app_join']} System update detected: Bot {{mention}} has joined the network."
+    ],
+    "leave": [
+        f"{_EMOJI['leave']} {{mention}} couldn't handle the Backdoor any longer...",
+        f"{_EMOJI['leave']} {{mention}} disconnected. Please check your internet connection and try again. (Error Code: 277)",
+        f"{_EMOJI['leave']} {{mention}} ran out of crucifixes, I guess...",
+        f"{_EMOJI['leave']} Poof! {{mention}} is gone. Probably rage-quit after groundskeeper bugged on them or something.",
+        f"{_EMOJI['leave']} {{mention}} saw the light. It was outside. Outside this challenge. They're happier now.",
+        f"{_EMOJI['leave']} {{mention}} opened a Dupe door."
+    ],
+    "kick": [
+        f"{_EMOJI['kick']} {{mention}} got kicked. Perhaps sold too many runs...",
+        f"{_EMOJI['kick']} {{mention}} slipped on a nanner peel and fell out of bounds! Oh? The nanner peel was placed by an admin? Ohhhh, ok, gotcha.",
+        f"{_EMOJI['kick']} An admin's mighty foot sent {{mention}} flying. A reminder that you shouldn't fly during the challenge. That's cheating."
+    ],
+    "ban": [
+        f"{_EMOJI['ban']} {{mention}} got electrocuted. Not by Surge, though.",
+        f"{_EMOJI['ban']} {{mention}} is now trapped out of next rooms with a groundskeeper.",
+        f"{_EMOJI['ban']} {{mention}} got figured out. By the admin."
+    ],
+    "kick_bot": [
+        f"{_EMOJI['app_leave']} Digital termination detected: Bot {{mention}} was kicked",
+        f"{_EMOJI['app_leave']} Error 403: Bot {{mention}} was forcibly removed.",
+        f"{_EMOJI['app_leave']} {{mention}} was disconnected by administrator action."
+    ],
+    "available": [
+        f"{_EMOJI['available']} **{{name}}** is now available to participate in the challenge *({{available_count}}/8)*"
+    ],
+    "available_ping": [
+        f"-# we have 8 people! <@{ROLES['available']}>, go to <#{CHANNELS['ps_link']}> before someone leaves again"
+    ],
+    "unavailable": [
+        f"{_EMOJI['unavailable']} **{{name}}** is no longer available *({{available_count}}/8)*"
+    ],
+    "unavailable_auto": [
+        f"{_EMOJI['unavailable']} **{{name}}** was inactive for too long, so they're marked as unavailable *({{available_count}}/8)*"
+    ],
+    "join_vc": [
+        f"{_EMOJI['join_vc']} **{{name}}** has joined the voice channel"
+    ],
+    "leave_vc": [
+        f"{_EMOJI['leave_vc']} **{{name}}** has left the voice channel"
+    ],
+    "promotion": [
+        f"{_EMOJI['promotion']} {{mention}} was promoted to **{{role}}**"
+    ],
+    "demotion": [
+        f"{_EMOJI['demotion']} {{mention}} was demoted"
+    ],
+    "birthday": [
+        f"{_EMOJI['birthday']} happy birthday, {{mention}}!!"
+    ],
+    "nuh_uh": [
+        "https://cdn.discordapp.com/attachments/715528165132599337/1442162843452440777/nuh-uh-3d-thumbnail-url-7g84og.png?ex=69246e4f&is=69231ccf&hm=b1bf1bb44ee89017d8404d35a1b0812eef3c6dc29a870ef3a8fefaa96fc7353e&"
+    ]
+}
+
+def message(dict_key: str, **kwargs) -> str:
+    import random
+    res = random.choice(_MESSAGES[dict_key])
+    res = res.format(**kwargs)
+    return res
---
+++ main.py
@@ -0,0 +1,368 @@
+import datetime
+
+import discord
+from discord.ext import commands
+from config import message, check_guild, TARGET_GUILD, ROLES, CHANNELS, TOKEN
+
+intents = discord.Intents.default()
+intents.members = True
+intents.presences = True
+intents.reactions = True
+intents.guilds = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+async def send(msg: str) -> None:
+    guild = bot.get_guild(TARGET_GUILD)
+    channel = guild.get_channel(CHANNELS['chat'])
+    if channel:
+        await channel.send(msg)
+
+
+
+def has_role(member: discord.Member, role_id: int) -> bool:
+    guild = member.guild
+    if not check_guild(guild.id):
+        return False
+
+    role = guild.get_role(role_id)
+    if not role:
+        return False
+
+    if role in member.roles:
+        return True
+    return False
+
+async def add_role(member: discord.Member, role_id: int) -> None:
+    guild = member.guild
+    if not check_guild(guild.id):
+        return
+
+    if not has_role(member, role_id):
+        role = guild.get_role(role_id)
+        await member.add_roles(role)
+
+async def remove_role(member: discord.Member, role_id: int) -> None:
+    guild = member.guild
+    if not check_guild(guild.id):
+        return
+
+    role = guild.get_role(role_id)
+    if not role:
+        return
+
+    if has_role(member, role_id):
+        role = guild.get_role(role_id)
+        await member.remove_roles(role)
+
+
+
+async def check_voice_state(member: discord.Member) -> None:
+    guild = member.guild
+    if not check_guild(guild.id):
+        return
+
+    vc = guild.get_channel(CHANNELS['vc'])
+    if not vc:
+        return
+
+    if member.voice and member.voice.channel == vc:
+        if not has_role(member, ROLES['in_vc']):
+            await send(message('join_vc', name=member.display_name))
+
+        if has_role(member, ROLES['leader']):
+            await add_role(member, ROLES['in_vc_leader'])
+        await add_role(member, ROLES['in_vc'])
+        await remove_role(member, ROLES['not_in_vc'])
+
+    else:
+        if has_role(member, ROLES['in_vc']):
+            await send(message('leave_vc', name=member.display_name))
+        await remove_role(member, ROLES['in_vc'])
+        await remove_role(member, ROLES['in_vc_leader'])
+        await add_role(member, ROLES['not_in_vc'])
+
+
+async def count_available_people() -> int:
+    guild = bot.get_guild(TARGET_GUILD)
+    channel = guild.get_channel(CHANNELS['availability'])
+    try:
+        msg = await channel.fetch_message(CHANNELS['availability_message'])
+    except discord.NotFound | discord.Forbidden:
+        return 0
+
+    found_reaction = None
+    for reaction in msg.reactions:
+        if reaction.emoji.id == CHANNELS['availability_reaction']:
+            found_reaction = reaction
+            break
+
+    res = 0
+    if found_reaction:
+        res = found_reaction.count
+    return res
+
+async def pure_check_availability_state(member: discord.Member) -> None:
+    guild = member.guild
+    if not check_guild(guild.id):
+        return
+
+    channel = guild.get_channel(CHANNELS['availability'])
+
+    try:
+        msg = await channel.fetch_message(CHANNELS['availability_message'])
+    except discord.NotFound | discord.Forbidden:
+        return
+
+    found_reaction = None
+    for reaction in msg.reactions:
+        if reaction.emoji.id == CHANNELS['availability_reaction']:
+            found_reaction = reaction
+            break
+    if found_reaction:
+        async for user in found_reaction.users():
+            if user == member:
+                if not has_role(member, ROLES['available']):
+                    await send(message('available', name=member.display_name, available_count=await count_available_people()))
+
+                if has_role(member, ROLES['leader']):
+                    await add_role(member, ROLES['available_leader'])
+                await add_role(member, ROLES['available'])
+                await remove_role(member, ROLES['not_available'])
+                return
+
+        if has_role(member, ROLES['available']):
+            await send(message('unavailable', name=member.display_name, available_count=await count_available_people()))
+
+        await remove_role(member, ROLES['available'])
+        await remove_role(member, ROLES['available_leader'])
+        await add_role(member, ROLES['not_available'])
+    else:
+        channel = bot.get_channel(CHANNELS['availability'])
+        msg = await channel.fetch_message(CHANNELS['availability_message'])
+        await msg.add_reaction(discord.PartialEmoji(id=CHANNELS['availability_reaction'], name='available'))
+
+
+async def add_availability(member: discord.Member) -> None:
+    guild = member.guild
+    if not check_guild(guild.id):
+        return
+
+    if not has_role(member, ROLES['available']):
+        await send(message('available', name=member.display_name, available_count=await count_available_people()))
+
+    if has_role(member, ROLES['leader']):
+        await add_role(member, ROLES['available_leader'])
+    await add_role(member, ROLES['available'])
+    await remove_role(member, ROLES['not_available'])
+    return
+
+async def remove_availability(member: discord.Member) -> None:
+    guild = member.guild
+    if not check_guild(guild.id):
+        return
+
+    if has_role(member, ROLES['available']):
+        await send(message('unavailable', name=member.display_name, available_count=await count_available_people()))
+
+    await remove_role(member, ROLES['available'])
+    await remove_role(member, ROLES['available_leader'])
+    await add_role(member, ROLES['not_available'])
+
+
+
+
+
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        if check_guild(guild.id):
+            status_message = await guild.get_channel(CHANNELS['chat']).send('bot restarted\n-# ...')
+            await status_message.edit(content=f'bot restarted\n-# *preparing*')
+            member_n = 0
+            for member in guild.members:
+                member_n += 1
+                await check_voice_state(member)
+                await pure_check_availability_state(member)
+                await status_message.edit(content=f'bot restarted\n-# *checking member {member_n}/{guild.member_count}*')
+            await status_message.edit(content=f'bot restarted\n-# *done*')
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    guild = member.guild
+    if not check_guild(guild.id):
+        return
+
+    vc = guild.get_channel(CHANNELS['vc'])
+    if not vc:
+        return
+
+    await check_voice_state(member)
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    if not check_guild(payload.guild_id):
+        return
+    if payload.message_id == CHANNELS['availability_message']:
+        emoji_id = payload.emoji.id
+        if emoji_id == CHANNELS['availability_reaction']:
+            await add_availability(bot.get_guild(payload.guild_id).get_member(payload.user_id))
+
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    if not check_guild(payload.guild_id):
+        return
+    if payload.message_id == CHANNELS['availability_message']:
+        emoji_id = payload.emoji.id
+        if emoji_id == CHANNELS['availability_reaction']:
+            await remove_availability(bot.get_guild(payload.guild_id).get_member(payload.user_id))
+
+@bot.event
+async def on_member_join(member):
+    guild = member.guild
+    if not check_guild(guild.id):
+        return
+
+    if member.bot:
+        await send(message('join_bot', mention=member.mention))
+        await add_role(member, ROLES['bot'])
+    else:
+        await send(message('join', mention=member.mention))
+        await send('-# hey there! check out <#1426974985402187776> <#1432401559672848507> <#1434653852367585300>')
+        for role in ROLES['new_people']:
+            await add_role(member, role)
+
+@bot.event
+async def on_member_remove(member):
+    guild = member.guild
+    if not check_guild(guild.id):
+        return
+
+    if member.bot:
+        await send(message('kick_bot', mention=member.mention))
+
+    else:
+        async for entry in guild.audit_logs(limit=1, action=discord.AuditLogAction.kick):
+            if entry.target == member and \
+                (discord.utils.utcnow() - entry.created_at).total_seconds() < 5:
+                    await send(message('kick', mention=member.mention))
+                    return
+
+        async for entry in guild.audit_logs(limit=1, action=discord.AuditLogAction.ban):
+            if entry.target == member and \
+                (discord.utils.utcnow() - entry.created_at).total_seconds() < 5:
+                    await send(message('ban', mention=member.mention))
+                    return
+
+        await send(message('leave', mention=member.mention))
+
+
+
+
+
+
+@bot.command()
+async def test(ctx):
+    await ctx.send('test pass')
+
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason: str = None):
+    if member == ctx.author:
+        return await ctx.send(message("nuh_uh"))
+    if member == ctx.guild.me:  # Prevent bot from kicking itself
+        return await ctx.send(message("nuh_uh"))
+    try:
+        await member.kick(reason=reason)
+    except discord.Forbidden:
+        await ctx.send(message("nuh_uh"))
+        print(f"Bot lacks permissions to kick {member.display_name} (ID: {member.id})")
+    except Exception as e:
+        await ctx.send(message("nuh_uh"))
+        print(f"Error kicking {member.display_name} (ID: {member.id}): {e}")
+
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason: str = None):
+    if member == ctx.author:
+        return await ctx.send(message("nuh_uh"))
+    if member == ctx.guild.me:  # Prevent bot from kicking itself
+        return await ctx.send(message("nuh_uh"))
+    try:
+        await member.ban(reason=reason)
+    except discord.Forbidden:
+        await ctx.send(message("nuh_uh"))
+        print(f"Bot lacks permissions to kick {member.display_name} (ID: {member.id})")
+    except Exception as e:
+        await ctx.send(message("nuh_uh"))
+        print(f"Error kicking {member.display_name} (ID: {member.id}): {e}")
+
+@bot.command()
+@commands.has_permissions(moderate_members=True)
+async def mute(ctx, member: discord.Member, duration: str, *, reason: str = None):
+    if member == ctx.author:
+        return await ctx.send(message("nuh_uh"))
+    if member == ctx.guild.me:
+        return await ctx.send(message("nuh_uh"))
+
+    timeout_duration = None
+    try:
+        if duration.endswith('s'):
+            seconds = int(duration[:-1])
+            timeout_duration = datetime.timedelta(seconds=seconds)
+        elif duration.endswith('m'):
+            minutes = int(duration[:-1])
+            timeout_duration = datetime.timedelta(minutes=minutes)
+        elif duration.endswith('h'):
+            hours = int(duration[:-1])
+            timeout_duration = datetime.timedelta(hours=hours)
+        elif duration.endswith('d'):
+            days = int(duration[:-1])
+            timeout_duration = datetime.timedelta(days=days)
+        elif duration.endswith('w'):
+            weeks = int(duration[:-1])
+            timeout_duration = datetime.timedelta(weeks=weeks)
+        else:
+            return await ctx.send("wrong duration format you moron")
+
+        if not timeout_duration: # Should be caught by the else above, but for safety
+            return await ctx.send("wrong duration format you moron")
+
+        if timeout_duration > datetime.timedelta(days=28):
+            return await ctx.send("you can't mute for more than 28 days because discord is a moron sorry")
+
+        await member.timeout(timeout_duration, reason=reason)
+        await ctx.send(f"muted the guy :white_check_mark:")
+
+    except ValueError:
+        return await ctx.send("wrong duration format you moron")
+    except discord.Forbidden:
+        await ctx.send(message("nuh_uh"))
+        print(f"Bot lacks permissions to timeout {member.display_name} (ID: {member.id})")
+    except Exception as e:
+        await ctx.send(message("nuh_uh"))
+        print(f"Error muting {member.display_name} (ID: {member.id}): {e}")
+
+@bot.command()
+@commands.has_permissions(moderate_members=True)
+async def unmute(ctx, member: discord.Member, *, reason: str = None):
+    if not member.is_timed_out():
+        return await ctx.send(message("nuh_uh"))
+
+    try:
+        await member.timeout(None, reason=reason) # Setting duration to None removes timeout
+        await ctx.send(f"unmuted :white_check_mark:")
+        print(f"Unmuted {member.display_name} (ID: {member.id}) by {ctx.author.display_name} for: {reason}")
+    except discord.Forbidden:
+        await ctx.send(message("nuh_uh"))
+        print(f"Bot lacks permissions to unmute {member.display_name} (ID: {member.id})")
+    except Exception as e:
+        await ctx.send(message("nuh_uh"))
+        print(f"Error unmuting {member.display_name} (ID: {member.id}): {e}")
+
+
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
